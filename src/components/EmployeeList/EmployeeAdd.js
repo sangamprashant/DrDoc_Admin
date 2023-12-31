304cb04914464c7ba11d3599cfde6507f82a2548
Prashant Srivastav <srivastavp891@gmail.com>
2024-01-01 00:09:37 +0530

    Job role and add employee

--- src/components/EmployeeList/EmployeeAdd.js
+++ src/components/EmployeeList/EmployeeAdd.js
@@ -1,11 +1,155 @@
-import React from 'react'
+import React, { useState } from "react";
 
 function EmployeeAdd() {
+  const [formData, setFormData] = useState({
+    personal: {
+      id: "",
+      name: "",
+      email: "",
+      dateOfBirth: "",
+      salary: "",
+      jobRole: "",
+      image: "",
+      preview: "",
+    },
+    address: {
+      address: "",
+      city: "",
+      phone: "",
+      currentAddress: "",
+      pin: "",
+      country: "",
+    },
+    bank: {
+      bankAccount: "",
+      accountHolderName: "",
+      ifcCode: "",
+      branch: "",
+    },
+  });
+
+  const handleChange = (category, field, value) => {
+    setFormData((prevData) => ({
+      ...prevData,
+      [category]: {
+        ...prevData[category],
+        [field]: value,
+      },
+    }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Handle form submission logic here
+    console.log("Form Data Submitted:", formData);
+  };
+
   return (
-    <div>
-      <h1>Employee Add</h1>
+    <div className="container">
+      <form onSubmit={handleSubmit}>
+        {/* Personal details */}
+        <h5>Personal details</h5>
+        <table className="table">
+          <thead>
+            <tr>
+              <th>Id</th>
+              <th>Name</th>
+              {/* ... other fields ... */}
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>
+                <input
+                  type="text"
+                  className="form-control"
+                  value={formData.personal.id}
+                  onChange={(e) => handleChange("personal", "id", e.target.value)}
+                />
+              </td>
+              <td>
+                <input
+                  type="text"
+                  className="form-control"
+                  value={formData.personal.name}
+                  onChange={(e) => handleChange("personal", "name", e.target.value)}
+                />
+              </td>
+              {/* ... other input fields ... */}
+            </tr>
+          </tbody>
+        </table>
+
+        {/* Address details */}
+        <h5>Address details</h5>
+        <table className="table">
+          <thead>
+            <tr>
+              <th>Address</th>
+              <th>City</th>
+              {/* ... other fields ... */}
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>
+                <input
+                  type="text"
+                  className="form-control"
+                  value={formData.address.address}
+                  onChange={(e) => handleChange("address", "address", e.target.value)}
+                />
+              </td>
+              <td>
+                <input
+                  type="text"
+                  className="form-control"
+                  value={formData.address.city}
+                  onChange={(e) => handleChange("address", "city", e.target.value)}
+                />
+              </td>
+              {/* ... other input fields ... */}
+            </tr>
+          </tbody>
+        </table>
+
+        {/* Bank details */}
+        <h5>Bank details</h5>
+        <table className="table">
+          <thead>
+            <tr>
+              <th>Bank Account</th>
+              <th>Account Holder Name</th>
+              {/* ... other fields ... */}
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>
+                <input
+                  type="text"
+                  className="form-control"
+                  value={formData.bank.bankAccount}
+                  onChange={(e) => handleChange("bank", "bankAccount", e.target.value)}
+                />
+              </td>
+              <td>
+                <input
+                  type="text"
+                  className="form-control"
+                  value={formData.bank.accountHolderName}
+                  onChange={(e) => handleChange("bank", "accountHolderName", e.target.value)}
+                />
+              </td>
+              {/* ... other input fields ... */}
+            </tr>
+          </tbody>
+        </table>
+
+        <button type="submit" className="btn btn-primary">Submit</button>
+      </form>
     </div>
-  )
+  );
 }
 
-export default EmployeeAdd
+export default EmployeeAdd;
--- src/components/EmployeeList/EmployeePost.js
+++ src/components/EmployeeList/EmployeePost.js
@@ -1,11 +1,59 @@
-import React from 'react'
+import React from "react";
 
 function EmployeePost() {
   return (
-    <div>
-      <h1>Employee Post</h1>
+    <div className="container">
+      <form className="my-5">
+        <h5 className="text-center text-capitalize">Add A new Post</h5>
+        <table className="table py-5">
+          <thead className="py-5">
+            <tr>
+              <td>
+                <input
+                  type="text"
+                  className="form-control"
+                  placeholder="Enter a new post.."
+                />
+              </td>
+              <td>
+                <input
+                  type="submit"
+                  className="form-control btn btn-outline-primary"
+                />
+              </td>
+            </tr>
+          </thead>
+        </table>
+      </form>
+      <h5 className="text-center text-capitalize">List of job Post's </h5>
+      <table className="table">
+        <thead>
+          <tr>
+            <th>Sr.no</th>
+            <th>Name</th>
+            <th>Action</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>1</td>
+            <td>Manager</td>
+            <td></td>
+          </tr>
+          <tr>
+            <td>2</td>
+            <td>Seller</td>
+            <td></td>
+          </tr>
+          <tr>
+            <td>3</td>
+            <td>Pa</td>
+            <td></td>
+          </tr>
+        </tbody>
+      </table>
     </div>
-  )
+  );
 }
 
-export default EmployeePost
+export default EmployeePost;
